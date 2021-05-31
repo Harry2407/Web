b27b93093f6f70c654fa80a7027cc18696e0a75c
Harry2407 <78526606+Harry2407@users.noreply.github.com>
2021-05-31 06:28:15 +0100

    Add files via upload

---
+++ home.php
@@ -0,0 +1,222 @@
+       
+        <!-- Start Header Section -->
+        <div class="banner">
+            <div class="overlay">
+                <div class="container">
+                    <div class="intro-text">
+                        <h1>Welcome To The <span>Online Library</span></h1>
+                        <p>Explore, Borrow Books and Keep the Admin's Money Running <br> Through Online</p> 
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Header Section -->
+        
+        
+        <!-- Start About Us Section -->
+    <section id="about-section" class="about-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-title text-center wow fadeInDown" data-wow-duration="2s" data-wow-delay="50ms">
+                        <h2>About Us</h2>
+                    </div>                        
+                </div>
+            </div>
+            <div class="row">
+               <div class="col-md-5">
+                   <div class="about-img">
+                       <img src="asset/images/corporate1.jpg" class="img-responsive" alt="About images">
+                       <div class="head-text">
+                           <p>Serah kau lah mo pinjam & baca buku apa, yang penting uang saya jalan.</p>
+                           <span>CEO, Mbahmu</span>
+                       </div>
+                   </div>
+               </div>
+               <div class="col-md-7">
+                   <div class="about-text">
+                       <p>bla bla bla bla bla bla bla bla bla bla.</p>
+                       <p>bla bla bla bla bla.</p>
+                   </div>
+                   
+                   <div class="about-list">
+                       <h4>Some important Feature</h4>
+                       <ul>
+                           <li><i class="fa fa-check-square"></i>Fitur Utama 1.</li>
+                           <li><i class="fa fa-check-square"></i>Fitur Utama 2.</li>
+                           <li><i class="fa fa-check-square"></i>Fitur Utama 3.</li>
+                           <li><i class="fa fa-check-square"></i>Fitur Utama 4.</li>
+                           <li><i class="fa fa-check-square"></i>Fitur Utama 5.</li>
+                           <li><i class="fa fa-check-square"></i>Fitur Utama 6.</li>
+                       </ul>
+                       
+                       <h4>More Feature</h4>
+                       <ul>
+                           <li><i class="fa fa-check-square"></i>Fitur Biasa 1.</li>
+                           <li><i class="fa fa-check-square"></i>Fitur Biasa 2.</li>
+                           <li><i class="fa fa-check-square"></i>Fitur Biasa 3.</li>
+                           <li><i class="fa fa-check-square"></i>Fitur Biasa 4.</li>
+                       </ul>
+                   </div>
+                   
+               </div>
+                
+                
+                
+            </div>
+        </div>
+    </section>
+        
+        
+        <!-- Start Call to Action Section -->
+    <section class="call-to-action">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 wow zoomIn" data-wow-duration="2s" data-wow-delay="300ms">
+                    <p>Awesome Aires Template is ready for <br> Business, Agency, Landing or Creative Portfolio.<br>Tapi Boong</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Call to Action Section -->
+        
+        
+        
+        
+        <!-- Start Team Member Section -->
+    <section id="team-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-title text-center wow fadeInDown" data-wow-duration="2s" data-wow-delay="50ms">
+                        <h2>Our Team</h2>
+                        <p>Tidak Kenal Tim Sendiri</p>
+                    </div>                        
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3 wow fadeInLeft" data-wow-duration="2s" data-wow-delay="300ms">
+                    <div class="team-member">
+                        <img src="asset/images/team/team-1.jpg" class="img-responsive" alt="">
+                        <div class="team-details">
+                            <h4>Matthew</h4>
+                            <p>Founder & Director</p>
+                            <ul>
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                                <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div><!-- /.col-md-3 -->
+                <div class="col-md-3 wow fadeInLeft" data-wow-duration="2s" data-wow-delay="600ms">
+                    <div class="team-member">
+                        <img src="asset/images/team/team-2.jpg" class="img-responsive" alt="">
+                        <div class="team-details">
+                            <h4>Mark</h4>
+                            <p>Founder & Director</p>
+                            <ul>
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                                <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div><!-- /.col-md-3 -->
+                <div class="col-md-3 wow fadeInLeft" data-wow-duration="2s" data-wow-delay="900ms">
+                    <div class="team-member">
+                        <img src="asset/images/team/team-3.jpg" class="img-responsive" alt="">
+                        <div class="team-details">
+                            <h4>Luke</h4>
+                            <p>Founder & Director</p>
+                            <ul>
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                                <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div><!-- /.col-md-3 -->
+                <div class="col-md-3 wow fadeInLeft" data-wow-duration="2s" data-wow-delay="1200ms">
+                    <div class="team-member">
+                        <img src="asset/images/team/team-4.jpg" class="img-responsive" alt="">
+                        <div class="team-details">
+                            <h4>John</h4>
+                            <p>Founder & Director</p>
+                            <ul>
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                                <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div><!-- /.col-md-3 -->
+            </div>
+        </div>
+    </section>
+    <!-- End Team Member Section -->
+        
+     
+        
+        <!-- Start Service Section -->
+        <section id="service-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="section-title text-center wow fadeInDown" data-wow-duration="2s" data-wow-delay="50ms">
+                            <h2>Our Books</h2>
+                            <p>Available Books in the Library</p>
+                        </div>                        
+                    </div>
+                </div>
+
+                        <!-- Start Service Section -->
+     
+                        <?php
+                    // `AccessionNo`, `BookTitle`, `BookDesc`, `Author`, `PublishDate`, `BookPublisher`, `Category`, `BookPrice`, `BookQuantity`, `Status`, `BookType`, `DeweyDecimal`, `OverAllQty`, `Donate`, `Remarks`
+                    
+                            // $mydb->setQuery("SELECT *, sum(BookQuantity) as qty FROM `tblbooks` GROUP BY BookTitle");
+                    $mydb->setQuery("SELECT * FROM `tblbooks` WHERE Status='Available' GROUP BY BookTitle"); 
+                            $cur = $mydb->loadResultlist();
+                            foreach ($cur as $result) {
+                                echo '<tr>'; 
+                                // echo '<td ><input type="checkbox" name="selector[]" id="selector[]" value="'.$result->SUBJ_ID. '"/>
+                                //      <a href="index.php?view=edit&id='.$result->SUBJ_ID.'">' . $result->SUBJ_CODE.'</a></td>';
+                                // echo '<td >' . $result->IBSN.'</td>';
+                                // echo '<td >'. $result->BookTitle.'</td>';
+                                // echo '<td>'.  $result->BookDesc.'</td>'; 
+                                // echo '<td>'. $result->Category.'</td>'; 
+                                // echo '<td>'. $result->Author.'</td>';
+                                // echo '<td>'. $result->BookPrice.'</td>';
+                                // echo '<td>'. $result->Status.'</td>';
+                                // echo '<td>'. $result->qty.'</td>';
+
+                                echo '<div class="col-md-3">
+                                            <div class="services-post">
+                                                <a href="index.php?q=borrow&id='.$result->IBSN.'"><i class="fa fa-book"></i></a>
+                                                <h2>'. $result->BookTitle.'</h2>
+                                                <p>'.  $result->BookDesc.'</p>
+                                            </div>
+                                        </div>';
+ 
+                      } 
+                    ?>
+
+
+                 
+                     
+                 
+            </div>
+        </section>
+        <!-- Start Service Section -->
+        
+        
+ 
